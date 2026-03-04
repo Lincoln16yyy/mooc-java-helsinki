a84b488852adb76081b5554631bd5336a2380959
Lincoln Gustavo De Brito Soares <kakuoriginal3@outlook.com>
2026-03-03 22:10:17 -0300

    part01-Part01_14.BooleanInput

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -5,8 +5,11 @@ public class BooleanInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write something:");
+        Boolean value= Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + value);
 
-        // write your program here
 
+        
     }
 }
